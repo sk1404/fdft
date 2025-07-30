bcd8c8e55498f09bffa11f3ae237c1f0a5190b25
sk1404 <sheshadri1404@gmail.com>
2025-07-30 11:50:06 +0530

    8_4coun.tcl

---
+++ 8_4coun.tcl
@@ -0,0 +1,14 @@
+set_attr lib_search_path /home/pgstudent14/counter_design_database_45nm/lib/
+set_attr hdl_search_path /home/pgstudent14/counter_design_database_45nm/rtl/
+set_attr library /home/pgstudent14/counter_design_database_45nm/lib/slow_vdd1v0_basicCells.lib
+read_hdl /home/pgstudent14/counter_design_database_45nm/simulation/engg/counter.v
+elaborate
+#read_sdc /home/pgstudent14/counter_design_database_45nm/simulation/rakeshvtv/count_s.sdc
+synthesize -to_mapped -effort medium
+write_hdl > net.v
+#write_sdc > dft_10_count_sdc.sdc
+#report_area > 4_bit_count_area.rep
+#report_gates > 4_bit_count_gates.rep
+#report_power > 4_bit_count_power.rep
+#report_timing > 4_bit_count_timing.rep
+gui_show
